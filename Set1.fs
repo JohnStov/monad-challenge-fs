6123d8051f9bead5a790b9573470a22bcadb0f5a
John Stovin <john.d.stovin@gmail.com>
2017-10-11 22:16:59 +0100

    Random character generation

--- Set1.fs
+++ Set1.fs
@@ -8,12 +8,25 @@ let rand s =
     s' , {unSeed = s'}
 
 let fiveRands () = 
-    let (v1, s1) = rand (mkSeed 1)
-    let (v2, s2) = rand s1
-    let (v3, s3) = rand s2
-    let (v4, s4) = rand s3
+    let v1, s1 = rand (mkSeed 1)
+    let v2, s2 = rand s1
+    let v3, s3 = rand s2
+    let v4, s4 = rand s3
     let v5 = fst (rand s4)
     [v1; v2; v3; v4; v5]
 
 let product vs =
     vs |> List.fold (*) (bigint 1)
+
+let toLetter (v : bigint) =
+    (char) (((int) 'a') + (int)(v % bigint 26))
+
+let randLetter s =
+    let (v, s') = rand s
+    toLetter v, s'
+
+let randString3 () =
+    let l1, s1 = randLetter (mkSeed 1)
+    let l2, s2 = randLetter s1
+    let l3 = fst (randLetter s2)
+    [l1; l2; l3] |> System.String.Concat
